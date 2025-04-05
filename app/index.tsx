ca663236928baa5068a5a2b5642f1509eccd34b2
SeprAY <julian.labanowski@wp.pl>
2025-04-05 19:17:30 +0200

    Routing test

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Moja pierwsza aplikacja React Native & Expo</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-dark-100 font-bold">Moja pierwsza aplikacja React Native & Expo</Text>
+      <Link href="/onboarding">Onboarding</Link>
+      <Link href="/movie/avengers">Avenger Movie</Link>
     </View>
   );
 }
