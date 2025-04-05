f43e7525710fa9b3afe76321e8a80d359777321e
SeprAY <julian.labanowski@wp.pl>
2025-04-05 21:28:32 +0200

    Added Navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,9 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-dark-100 font-bold">Moja pierwsza aplikacja React Native & Expo</Text>
-      <Link href="/onboarding">Onboarding</Link>
-      <Link href="/movie/avengers">Avenger Movie</Link>
     </View>
   );
 }
